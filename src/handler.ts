b2fe8370043903561afa0d4215c8af949e407280
alevny <alevnyacow@gmail.com>
2026-04-19 23:22:59 +0400

    feat: createUseCase, refactored error name

--- src/handler.ts
+++ src/handler.ts
@@ -2,9 +2,9 @@ import z, { ZodType } from "zod";
 import { Limiter, enrichDetails } from '@stompbox/limiter'
 import { zodErrorDetails } from '@stompbox/limiter/zod'
 
-enum HandlerErrorCodes {
-    INVALID_INPUT = 'SPRING-REVERB___INVALID-HANDLER-INPUT',
-    INVALID_OUTPUT = 'SPRING-REVERB___INVALID-HANDLER-OUTPUT'
+enum UseCaseErrorCodes {
+    INVALID_INPUT = 'SPRING-REVERB___INVALID-USE-CASE-INPUT',
+    INVALID_OUTPUT = 'SPRING-REVERB___INVALID-USE-CASE-OUTPUT'
 }
 
 export type Adapter<AdapterInput, AdapterOutput, HandlerInputSchema extends ZodType, HandlerOutputSchema extends ZodType> = {
@@ -12,7 +12,20 @@ export type Adapter<AdapterInput, AdapterOutput, HandlerInputSchema extends ZodT
     output: (x: z.infer<HandlerOutputSchema>, adapterInput: AdapterInput) => Promise<AdapterOutput>
 }
 
-export class HandlerError extends Limiter(HandlerErrorCodes) {}
+export class UseCaseError extends Limiter(UseCaseErrorCodes) {}
+
+export const createUseCase = <Input extends ZodType, Output extends ZodType>(
+    inputSchema: Input, 
+    outputSchema: Output,
+    handler: (x: z.infer<Input>) => Promise<z.infer<Output>>, 
+    sourceForErrorDetails?: string
+) => {
+    class UseCaseHandler extends UseCase(inputSchema, outputSchema, sourceForErrorDetails) {
+        executeRaw = handler
+    }
+
+    return new UseCaseHandler()
+}
 
 export const UseCase = <Input extends ZodType, Output extends ZodType>(inputSchema: Input, outputSchema: Output, sourceForErrorDetails?: string) => {
     abstract class HandlerClass {
@@ -25,7 +38,7 @@ export const UseCase = <Input extends ZodType, Output extends ZodType>(inputSche
         execute = async (input: z.infer<Input>): Promise<z.infer<Output>> => {
             const parsedInput = HandlerClass.inputSchema.safeParse(input)
             if (!parsedInput.success) {
-                throw new HandlerError('INVALID_INPUT', enrichDetails.withSource(sourceForErrorDetails)(
+                throw new UseCaseError('INVALID_INPUT', enrichDetails.withSource(sourceForErrorDetails)(
                     enrichDetails.withTimespamp(
                         zodErrorDetails(parsedInput.error)
                     )
@@ -34,7 +47,7 @@ export const UseCase = <Input extends ZodType, Output extends ZodType>(inputSche
             const output = await this.executeRaw(parsedInput.data)
             const parsedOutput = HandlerClass.outputSchema.safeParse(output)
             if (!parsedOutput.success) {
-                throw new HandlerError('INVALID_OUTPUT', enrichDetails.withSource(sourceForErrorDetails)(
+                throw new UseCaseError('INVALID_OUTPUT', enrichDetails.withSource(sourceForErrorDetails)(
                     enrichDetails.withTimespamp(
                         zodErrorDetails(parsedOutput.error))
                     )
